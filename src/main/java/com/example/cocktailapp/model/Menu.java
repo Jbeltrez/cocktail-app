2397353ab59bd1fe10e35ded538503e4bc125717
John Beltrez <beltrezjohn1@gmail.com>
2025-02-16 21:46:20 -0500

    menu model class establishes its relationship to drinks. MenuRepository created

--- src/main/java/com/example/cocktailapp/model/Menu.java
+++ src/main/java/com/example/cocktailapp/model/Menu.java
@@ -1,4 +1,23 @@
 package com.example.cocktailapp.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
 public class Menu {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    // One Menu can have many Drinks.
+    @OneToMany(mappedBy = "menu", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Drink> drinks = new HashSet<>();
 }
