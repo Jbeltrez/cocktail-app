9ed3207055cda2e468cf1231c2a5880e7b4ac3c1
John Beltrez <beltrezjohn1@gmail.com>
2025-02-16 22:10:06 -0500

    MenuController updated to reflect usage of MenuDTO

--- src/main/java/com/example/cocktailapp/controller/MenuController.java
+++ src/main/java/com/example/cocktailapp/controller/MenuController.java
@@ -1,13 +1,16 @@
 package com.example.cocktailapp.controller;
 
+import com.example.cocktailapp.dto.MenuDTO;
 import com.example.cocktailapp.model.Menu;
 import com.example.cocktailapp.repository.MenuRepository;
+import com.example.cocktailapp.service.MappingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/menus")
@@ -16,32 +19,41 @@ public class MenuController {
     @Autowired
     private MenuRepository menuRepository;
 
-    // GET endpoint to retrieve all menus
+    @Autowired
+    private MappingService mappingService;
+
+    // GET endpoint to retrieve all menus as DTOs
     @GetMapping
-    public List<Menu> getAllMenus() {
-        return menuRepository.findAll();
+    public List<MenuDTO> getAllMenus() {
+        List<Menu> menus = menuRepository.findAll();
+        return menus.stream()
+                .map(mappingService::mapToMenuDTO)
+                .collect(Collectors.toList());
     }
 
-    // GET endpoint to retrieve a single menu by ID
+    // GET endpoint to retrieve a single menu by ID as a DTO
     @GetMapping("/{id}")
-    public Menu getMenuById(@PathVariable Long id) {
-        return menuRepository.findById(id)
+    public MenuDTO getMenuById(@PathVariable Long id) {
+        Menu menu = menuRepository.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Menu not found"));
+        return mappingService.mapToMenuDTO(menu);
     }
 
-    // POST endpoint to create a new menu
+    // POST endpoint to create a new menu (returning DTO)
     @PostMapping
-    public Menu createMenu(@RequestBody Menu menu) {
-        return menuRepository.save(menu);
+    public MenuDTO createMenu(@RequestBody Menu menu) {
+        Menu savedMenu = menuRepository.save(menu);
+        return mappingService.mapToMenuDTO(savedMenu);
     }
 
-    // PUT endpoint to update a menu's name
+    // PUT endpoint to update a menu's name (returning DTO)
     @PutMapping("/{id}")
-    public Menu updateMenu(@PathVariable Long id, @RequestBody Menu menuDetails) {
+    public MenuDTO updateMenu(@PathVariable Long id, @RequestBody Menu menuDetails) {
         Menu menu = menuRepository.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Menu not found"));
         menu.setName(menuDetails.getName());
-        return menuRepository.save(menu);
+        Menu updatedMenu = menuRepository.save(menu);
+        return mappingService.mapToMenuDTO(updatedMenu);
     }
 
     // DELETE endpoint to delete a menu
